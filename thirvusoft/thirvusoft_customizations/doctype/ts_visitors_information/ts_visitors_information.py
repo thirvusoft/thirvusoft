6a9fa8403bc396cc8b815175ff644efd0f7b087b
SaraneshThrivu <saranesh@thrivusoft.in>
2021-12-24 12:35:01 +0530

    TS Visitors Information

---
+++ thirvusoft/thirvusoft_customizations/doctype/ts_visitors_information/ts_visitors_information.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2021, ThirvuSoft Private Limited and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from datetime import datetime
+import re
+
+class TSVisitorsInformation(Document):
+	def validate(self):
+		if(self.outtime != "00:00:00"):
+			hours = datetime.strptime(self.outtime, '%H:%M:%S') - datetime.strptime(self.intime, '%H:%M:%S')
+			shours = str(hours)
+			self.duration = shours
+		else:
+			pass
+		pattern = re.compile("(0|91)?[6-9][0-9]{9}")
+		if(pattern.match(self.contact)):
+			pass
+		else:
+			frappe.throw("Enter a valid number")
+		if(self.email != None):
+			if re.match(r"^[a-z0-9\.\+_-]+@[a-z0-9\._-]+\.[a-z]*$", self.email):
+				pass
+			else:
+				frappe.throw("Enter a valid Email ID")
