176d5477087afa161d2923748e3ca541a4a4882f
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2021-12-29 17:03:46 +0530

    Permission Doctype and Report V2.0

---
+++ thirvusoft/thirvusoft_customizations/doctype/ts_hourly_details/test_ts_hourly_details.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, ThirvuSoft Private Limited and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTSHourlyDetails(unittest.TestCase):
+	pass
---
+++ thirvusoft/thirvusoft_customizations/report/ts_hourly_details/ts_hourly_details.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2013, ThirvuSoft Private Limited and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from datetime import datetime
+def execute(filters=None):
+	filters=frappe._dict(filters or {})
+	columns, data = get_columns(), get_data(filters)
+	return columns, data
+def get_columns():
+	columns=[
+		{
+			'fieldname':'employee_id',
+			'fieldtype':'Data',
+			'label':_('Employee ID')
+		},
+		{
+			'fieldname':'date',
+			'fieldtype':'Date',
+			'label':_('Date')
+		},
+		{
+			'fieldname':'name',
+			'fieldtype':'Data',
+			'label':_('Name')
+		},
+		{
+			'fieldname':'start_time',
+			'fieldtype':'Time',
+			'label':_('Start Time')
+		},
+		{
+			'fieldname':'end_time',
+			'fieldtype':'Time',
+			'label':_('End Time')
+		},
+		{
+			'fieldname':'hours',
+			'fieldtype':'Data',
+			'label':_('Total Time')
+		},
+		{
+			'fieldname':'reason',
+			'fieldtype':'data',
+			'label':_('reason')
+		}
+	]
+	return columns
+
+
+
+def get_data(filters):
+	emp=frappe.get_all('TS Hourly Details')
+	if(filters.start_date and filters.end_date):
+		a=filters.start_date
+		b=filters.end_date
+		data=[]
+		for i in emp:
+			z=frappe.get_doc('TS Hourly Details',i.name)		
+			if(str(z.date)>=a and str(z.date)<=b):	
+				row={'employee_id':z.employee_id, 'date':z.date, 'name':z.employee_name, 
+				'start_time':z.start_time, 'end_time':z.end_time, 'reason':z.reason, 'hours':z.hours}
+				data.append(row)
+		return data
+
