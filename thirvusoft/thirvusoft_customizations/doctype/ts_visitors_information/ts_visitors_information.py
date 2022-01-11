00a9d81da379ea228b644386b130e8d84989cef1
SaraneshThrivu <saranesh@thrivusoft.in>
2022-01-11 15:46:51 +0530

    changes done in ts visitor info py file

--- thirvusoft/thirvusoft_customizations/doctype/ts_visitors_information/ts_visitors_information.py
+++ thirvusoft/thirvusoft_customizations/doctype/ts_visitors_information/ts_visitors_information.py
@@ -10,15 +10,14 @@ class TSVisitorsInformation(Document):
 	def validate(self):
 		if(self.outtime != "00:00:00"):
 			duration = datetime.strptime(self.outtime, '%H:%M:%S') - datetime.strptime(self.intime, '%H:%M:%S')
-			sduration = str(duration)
-			self.duration = sduration
+			self.duration = str(duration)
 		else:
 			pass
 		pattern = re.compile("(0|91)?[6-9][0-9]{9}")
 		if(pattern.match(self.contact)):
 			pass
 		else:
-			frappe.throw("Enter a valid number")
+			frappe.throw("Enter a valid contact number")
 		if(self.email != None):
 			if re.match(r"^[a-z0-9\.\+_-]+@[a-z0-9\._-]+\.[a-z]*$", self.email):
 				pass
