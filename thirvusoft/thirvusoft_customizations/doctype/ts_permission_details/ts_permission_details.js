b6d3fa4d28c2695eae23222c859347935d7bacee
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-01-12 09:35:45 +0530

    TS Permission details (Permission role updated)

--- thirvusoft/thirvusoft_customizations/doctype/ts_permission_details/ts_permission_details.js
+++ thirvusoft/thirvusoft_customizations/doctype/ts_permission_details/ts_permission_details.js
@@ -1,14 +1,13 @@
 // Copyright (c) 2022, ThirvuSoft Private Limited and contributors
 // For license information, please see license.txt
-function timediff(frm,cdt,cdn){
-	let row = locals[cdt][cdn];
-		var starttimeString = row.start_time;
+function timediff(frm){
+		var starttimeString = frm.doc.start_time;
 		var H = +starttimeString.substr(0, 2);
 		var h = H % 12 || 12;
 		var ampm = (H < 12 || H === 24) ? "AM" : "PM";
 		starttimeString = h + starttimeString.substr(2, 3)+starttimeString.substr(5,5) + ampm;
 
-		var endtimeString = row.end_time;
+		var endtimeString = frm.doc.end_time;
 		var H = +endtimeString.substr(0, 2);
 		var h = H % 12 || 12;
 		var ampm = (H < 12 || H === 24) ? "AM" : "PM";
@@ -21,14 +20,14 @@ function timediff(frm,cdt,cdn){
 		var hours = parseInt(duration.asHours());
 		var minutes = parseInt(duration.asMinutes())%60;
 		
-		frappe.model.set_value(cdt, cdn, "hours",
+		cur_frm.set_value("hours",
 		hours +' Hours '+ minutes+' Minutes ');
 }
 frappe.ui.form.on('TS Permission Details', {
-	end_time:function(frm, cdt, cdn){
-		timediff(frm,cdt,cdn)
+	end_time:function(frm){
+		timediff(frm)
 	},
-	start_time:function(frm, cdt, cdn){
-		timediff(frm,cdt,cdn)
+	start_time:function(frm){
+		timediff(frm)
 	}
 });
