9b95c53346e6cfe0801f83c3c933bcf9eccf61ce
RealThirvu4You <sibi.kumar@thirvusoft.in>
2022-03-17 09:16:55 +0530

    create workflow and email notification

--- thirvusoft/thirvusoft_customizations/custom/python/install.py
+++ thirvusoft/thirvusoft_customizations/custom/python/install.py
@@ -20,3 +20,152 @@ def create_custom_meeting():
         new_doc = frappe.new_doc('TS Meeting Type')
         new_doc.ts_meeting_type = row
         new_doc.save()
+
+# def create_jobApplicant_workflow():
+#     if frappe.db.exists('Workflow', 'Job Applicant workflow'):
+#         frappe.delete_doc('Workflow', 'Job Applicant workflow')
+#     if not frappe.db.exists('Role', 'Receptionist'):
+#         frappe.get_doc(dict(doctype='Role',
+#             role_name='Receptionist')).insert(ignore_if_duplicate=True)
+#     workflow = frappe.new_doc('Workflow')
+#     workflow.workflow_name = 'Job Applicant workflow'
+#     workflow.document_type = 'Job Applicant'
+#     workflow.workflow_state_field = 'workflow_state'
+#     workflow.is_active = 1
+#     workflow.send_email_alert = 1
+#     workflow.append('states', dict(
+#         state = 'Draft', allow_edit = 'Receptionist',update_field = 'status', update_value = 'open'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Initial Data Validation', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Initial Data Validation'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Selected For Telephoneic Interview', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Selected For Telephoneic Interview'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Rejected', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Rejected'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Telephonic Interview', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Telephonic Interview'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Selected For In-Person Interview', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Selected For In-Person Interview'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Follow Up', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Follow Up'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Written Test', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Written Test'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Selected For HR Round', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Selected For HR Round'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'HR Round', allow_edit = 'Receptionist',update_field = 'status', update_value = 'HR Round'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Selected For Technical Round', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Selected For technical Round'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Technical Round', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Technical Round'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Final Round', allow_edit = 'Receptionist',update_field = 'status', update_value = 'Final Round'
+#     ))
+#     workflow.append('states', dict(
+#         state = 'Selected For Employee', allow_edit = 'Receptionist',update_field = 'status', update_value = 'shortlisted'
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Draft', action='Initial Data Validation', next_state = 'Initial Data Validation',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Initial Data Validation', action='Selected For Telephoneic Interview', next_state = 'Telephonic Interview',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Initial Data Validation', action='Reject', next_state = 'Rejected',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Telephonic Interview', action='Selected For In-Person Interview', next_state = 'In-Person Interview',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'In-Person Interview', action='Follow Up', next_state = 'Follow Up',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Telephonic Interview', action='Reject', next_state = 'Rejected',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Follow Up', action='Selected For In-Person Interview', next_state = 'In-Person Interview',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Follow Up', action='Reject', next_state = 'Rejected',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'In-Person Interview', action='Written Test', next_state = 'Written Test',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'In-Person Interview', action='Reject', next_state = 'Rejected',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Written Test', action='Selected For HR Round', next_state = 'HR Round',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+
+
+#     workflow.append('transitions', dict(
+#         state = 'Technical Round', action='HR Round', next_state = 'HR Round',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Technical Round', action='Reject', next_state = 'Draft',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'HR Round', action='Document Verification', next_state = 'Document Verification',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'HR Round', action='Reject', next_state = 'Draft',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Document Verification', action='Call Letter', next_state = 'Call Letter',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Document Verification', action='Reject', next_state = 'Draft',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Call Letter', action='On Board', next_state = 'On Board',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.append('transitions', dict(
+#         state = 'Call Letter', action='Reject', next_state = 'Draft',
+#         allowed='Receptionist', allow_self_approval= 1
+#     ))
+#     workflow.insert(ignore_permissions=True)
+#     return workflow
+# def create_state():
+#     list=[""]
+#     for row in list:
+#         if not frappe.db.exists('Workflow State', row):
+#             new_doc = frappe.new_doc('Workflow State')
+#             new_doc.workflow_state_name = row
+#             new_doc.save()
+# def create_action():
+#     list=[""]
+#     for row in list:
+#         if not frappe.db.exists('Workflow Action Master', row):
+#             new_doc = frappe.new_doc('Workflow Action Master')
+#             new_doc.workflow_action_name = row
+#             new_doc.save()
---
+++ thirvusoft/thirvusoft_customizations/notification/selected_for_in_person/selected_for_in_person.md
@@ -0,0 +1,23 @@
+Dear {{doc.applicant_name}},
+
+Greeting from Thirvu Soft Private Limited 
+
+This is a response to your job application for the profile of {{doc.position_applied}} wherein you expressed interest in employment with Thirvu Soft Private Limited. After going through your application, we have found you suitable for this job and therefore, we have arranged for a face-to-face interview.
+
+We would like to invite you for the interview scheduled on [Date] around [Time] at our office. 
+
+Please bring your profile along with the Covid vaccinated certificate on the date of the interview.
+
+Find the location details available below,
+Thirvu Soft Private Limited
+2B, Bushido Towers, Nava India Road, Sowripalayam Village, Peelamedu, Coimbatore, Tamil Nadu 641028
+Plus code : 2X9V+RH Coimbatore, Tamil Nadu
+Kindly confirm your invitation acceptance via mail.
+
+
+
+Regards,
+Myvizhi Palanisamy
+HR Manager
+Thirvu Soft Private Limited 
+
