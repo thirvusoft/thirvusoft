113c2df9adbc8754c4fedeefefb1056aad6f0807
creative-paramu <paramukalyani@gmail.com>
2022-03-22 15:12:34 +0530

    MoM Customization

--- thirvusoft/thirvusoft_customizations/doctype/ts_minutes_of_meeting/ts_minutes_of_meeting.js
+++ thirvusoft/thirvusoft_customizations/doctype/ts_minutes_of_meeting/ts_minutes_of_meeting.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('TS Minutes of Meeting', {
-	// refresh: function(frm) {
-
-	// }
+	project: function(frm, cdt, cdn) {
+		let p= locals[cdt][cdn]	
+		let l=frappe.db.get_value("Project", p.project, ["client_name","client_contact_number"]).then(function(f){
+			frappe.model.set_value(cdt,cdn,'client',f.message.client_name)
+			frappe.model.set_value(cdt,cdn, 'client_contact_number',f.message.client_contact_number)
+		})
+		
+		// console.log(l)
+		// 		frm.add_fetch("client","client_name","client_name");
+		// frm.add_fetch("client","client_contact_number","client_contact_number");
+	 }
 });
