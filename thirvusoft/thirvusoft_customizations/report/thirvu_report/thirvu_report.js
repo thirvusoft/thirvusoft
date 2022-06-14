6b8df3145a25563a25776e60682a4ffaffd4632d
unstopablesathish <sathish.kumar@thirvusoft.in>
2022-06-14 18:07:01 +0530

    Feat:Columns & Filters

--- thirvusoft/thirvusoft_customizations/report/thirvu_report/thirvu_report.js
+++ thirvusoft/thirvusoft_customizations/report/thirvu_report/thirvu_report.js
@@ -4,6 +4,28 @@
 
 frappe.query_reports["Thirvu Report"] = {
 	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default" :frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default" :frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+            "fieldname": "Project",
+            "label": __("Project"),
+            "fieldtype": "Link",
+            "options": "Project",
+            "reqd": 1
+        },
+		
 
 	]
 };
--- thirvusoft/thirvusoft_customizations/report/thirvu_report/thirvu_report.py
+++ thirvusoft/thirvusoft_customizations/report/thirvu_report/thirvu_report.py
@@ -3,6 +3,67 @@
 
 # import frappe
 
+from ast import Return
+from frappe.utils import data
+from frappe import _
+
+
 def execute(filters=None):
 	columns, data = [], []
+	columns = get_columns()
+	data = get_data(filters)
 	return columns, data
+
+def get_columns():
+	columns = [
+		{
+			"label": _("Task"),
+			"fieldtype": "Link",
+			"fieldname": "task",
+			"options": "Task",
+			"width": 300
+		},
+		{
+			"label": _("Subject"),
+			"fieldtype": "Data",
+			"fieldname": "subject",
+			"width": 300
+		},
+		{
+			"label": _("Task Type"),
+			"fieldtype": "Link",
+			"fieldname": "task_type",
+			"options": "Task Type",
+			"width": 150
+		},
+		{
+			"label": _("Status"),
+			"fieldtype": "Data",
+			"fieldname": "status",
+			"width": 150
+		},
+		{
+			"label": _("Hours Taken"),
+			"fieldtype": "Float",
+			"fieldname": "hours_taken",
+			"width": 150
+		},
+		{
+			"label": _("Resource Count"),
+			"fieldtype": "Data",
+			"fieldname": "resource_count",
+			"width": 150
+		},
+	]
+
+	return columns
+
+def get_data(filters):
+	return [{
+		'task' : 'TASK-2022-01245',
+		'subject' : 'Forgot Password',
+		'task_type' : 'Medium',
+		'status' : 'Completed',
+		'hours_taken' : 5,
+		'resource_count' : 2
+	}]
