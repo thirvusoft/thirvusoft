5e45cdc6fae34ac9e5563d9a8d1c07a678dd1341
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-06-14 17:05:11 +0530

    Feat: Thirvu report created

---
+++ thirvusoft/thirvusoft_customizations/report/thirvu_report/thirvu_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, ThirvuSoft Private Limited and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Thirvu Report"] = {
+	"filters": [
+
+	]
+};
---
+++ thirvusoft/thirvusoft_customizations/report/thirvu_report/thirvu_report.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, ThirvuSoft Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
