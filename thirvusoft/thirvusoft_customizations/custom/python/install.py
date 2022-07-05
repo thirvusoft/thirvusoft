d0c616c7e84edafa322257883e97d8dfeb6af0da
NaviN <navin@thirvusoft.in>
2022-07-05 11:39:19 +0530

    Leave Application workflow

--- thirvusoft/thirvusoft_customizations/custom/python/install.py
+++ thirvusoft/thirvusoft_customizations/custom/python/install.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from thirvusoft.thirvusoft_customizations.utils.hr.leave_application.leave_application_custom_fields import leave_application_customization
 import frappe
 def after_install():
     create_custom_role()
@@ -8,6 +9,7 @@ def after_install():
     create_action()
     create_jobApplicant_workflow()
     create_leave_application_workflow()
+    leave_application_customization()
 
 def create_custom_role():
     existing_doc = frappe.db.get_value('Role', {'role_name': 'Receptionist'}, 'name')
@@ -166,11 +168,21 @@ def create_jobApplicant_workflow():
     workflow.insert(ignore_permissions=True)
     return workflow
 def create_state():
-    list=["Open","Approved by Tech Lead","Approved","Draft","Initial Data Validation","Selected For Telephoneic Interview","In-Person Interview","Rejected","Telephonic Interview","Selected For In-Person Interview","Follow Up","Written Test","Selected For HR Round","HR Round","Selected For Technical Round","Technical Round","Final Round","Selected For Employee"]
+    list=["Open","Approved by Tech Lead","Rejected by Tech Lead","Approved by HR Manager","Rejected by HR Manager","Approved","Draft","Initial Data Validation","Selected For Telephoneic Interview","In-Person Interview","Rejected","Telephonic Interview","Selected For In-Person Interview","Follow Up","Written Test","Selected For HR Round","HR Round","Selected For Technical Round","Technical Round","Final Round","Selected For Employee"]
     for row in list:
         if not frappe.db.exists('Workflow State', row):
             new_doc = frappe.new_doc('Workflow State')
             new_doc.workflow_state_name = row
+            if(row=="Open"):
+                new_doc.style="Primary"
+            if(row=="Approved by Tech Lead"):
+                new_doc.style="Warning"
+            if(row=="Rejected by Tech Lead"):
+                new_doc.style="Danger"
+            if(row=="Rejected by HR Manager"):
+                new_doc.style="Danger"
+            if(row=="Approved by HR Manager"):
+                new_doc.style="Success"
             new_doc.save()
 def create_action():
     list=["Initial Data Validation","Approve","Reject","Selected For Telephoneic Interview","Reject","Selected For In-Person Interview","Follow Up","Written Test","Selected For HR Round","Selected For Technical Round","selected For Final Round","Selected For Employee","shortlisted"]
@@ -186,19 +198,22 @@ def create_leave_application_workflow():
     workflow.workflow_name = 'Leave Application Flow'
     workflow.document_type = 'Leave Application'
     workflow.workflow_state_field = 'workflow_state'
-    workflow.is_active = 1
+    workflow.is_active = 0
     workflow.send_email_alert = 1
     workflow.append('states', dict(
         state = 'Open', allow_edit = 'All',
     ))
     workflow.append('states', dict(
-        state = 'Approved by Tech Lead', allow_edit = 'HR Manager'
+        state = 'Approved by Tech Lead', allow_edit = 'Tech Lead'
     ))
     workflow.append('states', dict(
-        state = 'Approved', allow_edit = 'HR Manager', 	doc_status = 1
+        state = 'Rejected by Tech Lead', allow_edit = 'Tech Lead'
     ))
     workflow.append('states', dict(
-        state = 'Rejected', allow_edit = 'HR Manager'
+        state = 'Approved by HR Manager', allow_edit = 'HR Manager', 	doc_status = 1
+    ))
+    workflow.append('states', dict(
+        state = 'Rejected by HR Manager', allow_edit = 'HR Manager'
     ))
     
     workflow.append('transitions', dict(
@@ -206,16 +221,16 @@ def create_leave_application_workflow():
         allowed='Tech Lead', allow_self_approval= 1
     ))
     workflow.append('transitions', dict(
-        state = 'Open', action='Reject', next_state = 'Rejected',
-        allowed='Tech Lead', allow_self_approval= 1, condition='doc.status == "Rejected" and doc.reason_for_rejected'
+        state = 'Open', action='Reject', next_state = 'Rejected by Tech Lead',
+        allowed='Tech Lead', allow_self_approval= 1,
     ))
     workflow.append('transitions', dict(
-        state = 'Approved by Tech Lead', action='Approve', next_state = 'Approved',
+        state = 'Approved by Tech Lead', action='Approve', next_state = 'Approved by HR Manager',
         allowed='HR Manager', allow_self_approval= 1
     ))
     workflow.append('transitions', dict(
-        state = 'Approved by Tech Lead', action='Reject', next_state = 'Rejected',
-        allowed='HR Manager', allow_self_approval= 1, condition='doc.status == "Rejected" and doc.reason_for_rejected'
+        state = 'Approved by Tech Lead', action='Reject', next_state = 'Rejected by HR Manager',
+        allowed='HR Manager', allow_self_approval= 1,
     ))
    
     workflow.insert(ignore_permissions=True)
--- thirvusoft/thirvusoft_customizations/custom/python/leave_application.py
+++ thirvusoft/thirvusoft_customizations/custom/python/leave_application.py
@@ -1,9 +1,13 @@
-import frappe
 from erpnext.hr.doctype.leave_application.leave_application import LeaveApplication
-
+import frappe
 class status_change(LeaveApplication):
     def on_submit(self):
-        if self.status in ["Open", "Cancelled"]:
-            self.status = "Approved"
-        self.validate_back_dated_application()
-        self.update_attendance()
+        if(self.hr_manager_approved == 1):
+            if self.status in ["Approved by HR Manager"]:
+                self.status = "Approved"
+                self.validate_back_dated_application()
+                self.update_attendance()
+            if self.status in ["Rejected by HR Manager","Rejected by Tech Lead"]:
+                self.status = "Cancelled"
+        else:
+            frappe.throw("Needs Approval from HR Manager")
