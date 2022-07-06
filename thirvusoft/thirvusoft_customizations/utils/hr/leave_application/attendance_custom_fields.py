37cbb9cc6bdf3c83b6eecca45120199b7b038870
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2022-07-06 17:26:21 +0530

    TS Compensation Requset Doctype date field validation and add extra compensation field in attendance doctype , write code for compensation workflow

---
+++ thirvusoft/thirvusoft_customizations/utils/hr/leave_application/attendance_custom_fields.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+def attendance_custom_fields():
+    custom_fields = {
+        "Attendance": [
+            dict(
+                fieldname="compensation",
+                fieldtype="Check",
+                label="Is Compensation",
+                insert_after="company",
+                readonly=1,
+               
+            ),
+           
+            dict(fieldname='compensation_day', label='Compensation',
+                 fieldtype='Link', options='TS Compensation Request', insert_after='compensation' ,readonly=1),
+  
+  
+        ]
+    }
+    create_custom_fields(custom_fields)
