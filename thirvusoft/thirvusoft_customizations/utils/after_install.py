a446f6e6bac585d89849317781b231f5183b32a1
root <root@ip-172-31-19-14.ap-south-1.compute.internal>
2022-07-14 11:56:21 +0000

    Job Application

--- thirvusoft/thirvusoft_customizations/utils/after_install.py
+++ thirvusoft/thirvusoft_customizations/utils/after_install.py
@@ -1,3 +1,4 @@
+
 from thirvusoft.thirvusoft_customizations.utils.support.issue import issue_customization
 from thirvusoft.thirvusoft_customizations.utils.crm.lead import customize
 from thirvusoft.thirvusoft_customizations.utils.user.role.roles import create_role
@@ -7,3 +8,5 @@ def after_install():
     issue_customization()
     customize()
     create_role()
+
+
