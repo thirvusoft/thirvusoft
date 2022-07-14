60bb7334408552af7623d42c185e5dcfcb818e1c
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-07-14 12:06:05 +0530

    Delete ts_compensation_request.py

--- thirvusoft/thirvusoft_customizations/doctype/ts_compensation_request/ts_compensation_request.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022, ThirvuSoft Private Limited and contributors
-# For license information, please see license.txt
-
-import frappe
-from frappe.model.document import Document
-
-class TSCompensationRequest(Document):
-    pass
-
